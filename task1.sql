f42990a5636ebc6f5ea4ecc72e7c3db600507bf3
Baris <kzkzkkzkz>
2022-06-06 17:05:32 -0400

    upload

--- task1.sql
+++ task1.sql
@@ -1,1 +1,9 @@
 SELECT title,description FROM film;
+
+SELECT * FROM film WHERE length>60 AND length<75;
+
+SELECT * FROM film WHERE  rental_rate=0.99; AND (replacement_cost=12.99 OR replacement_cost=28.99);
+
+SELECT last_name FROM customer WHERE first_name='Mary';
+
+SELECT * FROM film WHERE length<=50 and NOT rental_rate   in (2.99,4.99)
