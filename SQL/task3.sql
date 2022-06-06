cbd10824042d073f02d406aff09a8d9f20e8e908
Baris <kzkzkkzkz>
2022-06-06 18:05:57 -0400

    upload

---
+++ SQL/task3.sql
@@ -0,0 +1,7 @@
+SELECT country FROM  WHERE country LIKE  'A'%'a';
+
+SELECT country FROM country WHERE country LIKE '___n';
+
+SELECT title FROM film WHERE title ILIKE '%T%T%T%T%';
+
+SELECT title FROM film WHERE title LIKE 'C%' AND length>90 AND rental_rate=2.99;
