f8a00782f3cd31c3a1c5d6fd8b365fdd7bffce43
Baris <kzkzkkzkz>
2022-06-15 17:56:46 -0400

    upload

---
+++ SQL/src/task5.sql
@@ -0,0 +1,4 @@
+SELECT DISTINCT  replacement_cost FROM film;
+SELECT COUNT (DISTINCT replacement_cost) FROM film WHERE title LIKE 'T%' AND rating='G';
+SELECT COUNT (country) FROM country WHERE LENGTH (country)=5;
+SELECT COUNT(city) FROM city WHERE city LIKE '%r';
